78e980adcda1479fbbfbbfe267fe50d8c2101da7
RicoViking9000 <38385704+RicoViking9000@users.noreply.github.com>
2019-04-27 16:43:38 -0400

    start disguard, not disguard beta

--- Bot/Disguard.py
+++ Bot/Disguard.py
@@ -26,10 +26,10 @@ async def on_ready(): #Method is called whenever bot is ready after connection/r
     booted = True
     print("Booted")
     await bot.change_presence(status=discord.Status.online, activity=discord.Game("At attention"))
-#database.Initialize(secure.token())
-#bot.run(secure.token()) #Bot token stored in another file, otherwise anyone reading this could start the bot
-database.Initialize(secure.beta())
-bot.run(secure.beta())
+database.Initialize(secure.token())
+bot.run(secure.token()) #Bot token stored in another file, otherwise anyone reading this could start the bot
+#database.Initialize(secure.beta())
+#bot.run(secure.beta())
 
 @bot.command()
 async def verify(ctx):
