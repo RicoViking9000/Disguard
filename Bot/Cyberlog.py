9c68d42d97d596f4e29b64e24f1b138089d35483
RicoViking9000 <ricoviking9000@gmail.com>
2019-05-26 22:06:06 -0400

    patch 0.2.8

--- Bot/Cyberlog.py
+++ Bot/Cyberlog.py
@@ -81,7 +81,6 @@ class Cyberlog(commands.Cog):
         c = database.GetLogChannel(g, 'message')
         try: message = payload.cached_message
         except: message = None
-        print(message)
         if message is not None and pauseDelete > 0 and message.guild == serverDelete:
             pauseDelete -= 1
             return
