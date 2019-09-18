48ae8f03247123c777374a755f3e60182e8e9591
RicoViking9000 <ricoviking9000@gmail.com>
2019-09-18 16:25:16 -0400

    patch 0.2.14.6
    •Actually fixed the issue where new members and new servers didn't populate new members in the database. For new servers, this resulted in errors when attempting to configure the logging module online
    •Fixed many bugs relating to members and database management

--- Bot/database.py
+++ Bot/database.py
@@ -78,7 +78,6 @@ async def VerifyServer(s: discord.Guild, b: commands.Bot):
         return
     spam = None
     log = None
-    members = None
     if serv is not None:
         spam = serv.get("antispam") #antispam object from database
         log = serv.get("cyberlog") #cyberlog object from database
@@ -149,31 +148,26 @@ async def VerifyServer(s: discord.Guild, b: commands.Bot):
     for m in s.members: #Create dict 
         membDict[str(m.id)] = m.name
         membDict[m.name] = m.id
+    if (await servers.find_one({'server_id': s.id})).get('members') is None or (await servers.find_one({'server_id': s.id})) is None: await servers.update_one({'server_id': s.id}, {'$set': {'members': []}}, True)
     for id in membIDs:
-        member=None
-        if members is not None:
-            for m in members:
-                if m.get('id') == id:
-                    member=m
-                    break
-    for m in (await servers.find_one({'server_id': s.id})).get('members'):
-        if m.get('id') in membIDs or (await servers.find_one({'server_id': s.id})).get('members') is None or (await servers.find_one({'server_id': s.id})) is None:
-            try:
+        if (await servers.find_one({'server_id': s.id})).get('members') is None or (await servers.find_one({'server_id': s.id})) is None or (await servers.find_one({'server_id': s.id, 'members.id': id})) is None:
+            await servers.update_one({"server_id": s.id}, {"$push": { 'members': {
+                'id': id,
+                'name': membDict.get(str(id)),
+                'warnings': spam.get('warn'),
+                'quickMessages': [],
+                'lastMessages': []}}}, True)
+    else:
+        for member in (await servers.find_one({'server_id': s.id})).get('members'):
+            if member.get('id') in membIDs:
                 await servers.update_one({"server_id": s.id, "members.id": id}, {"$set": {
-                    "members.$.id": id,
-                    "members.$.name": membDict.get(str(id)),
+                    "members.$.id": m.get('id'),
+                    "members.$.name": membDict.get(str(m.get('id'))),
                     "members.$.warnings": spam.get('warn') if member is None else member.get('warnings'),
                     "members.$.quickMessages": [] if member is None or member.get('quickMessages') is None else member.get('quickMessages'),
                     "members.$.lastMessages": [] if member is None or member.get('lastMessages') is None else member.get('lastMessages')
                 }}, True)
-            except: #new member joined since last database refresh
-                await servers.update_one({'server_id': s.id}, {'$push': { 'members': {
-                    'id': id,
-                    'name': membDict.get(str(id)),
-                    'warnings': spam.get('warn'),
-                    'quickMessages': [],
-                    'lastMessages': []}}})
-        else: await servers.update_one({'server_id': s.id}, {'$pull': {'members': {'id': m.get('id')}}})
+            else: await servers.update_one({'server_id': s.id}, {'$pull': {'members': {'id': m.get('id')}}})
 
 async def VerifyUsers(b: commands.Bot):
     '''Ensures every global Discord user in a bot server has one unique entry. No use for these variables at the moment; usage to come'''
