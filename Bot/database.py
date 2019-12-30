c84be4d03888311ac7bbc25f9e9566e0c5011380
RicoViking9000 <ricoviking9000@gmail.com>
2019-12-30 14:00:34 -0500

    patch 0.2.14.15
    bug fix

--- Bot/database.py
+++ Bot/database.py
@@ -188,7 +188,7 @@ async def VerifyUsers(b: commands.Bot):
     
 async def VerifyUser(m: discord.Member, b: commands.Bot):
     '''Ensures that an individual user is in the database, and checks its variables'''
-    if verifications.get(s.id) is not None and (datetime.datetime.now() - verifications.get(s.id)).seconds < 600: return
+    if verifications.get(m.guild.id) is not None and (datetime.datetime.now() - verifications.get(m.guild.id)).seconds < 600: return
     print('Verifying member: {} - {} in server {} - {}'.format(m.name, m.id, m.guild.name, m.guild.id))
     if b.get_user(m.id) is None: await users.delete_one({'user_id': m.id})
     else: await users.update_one({"user_id": m.id}, {"$set": { #update database
