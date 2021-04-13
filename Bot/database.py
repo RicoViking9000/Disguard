e4814ec8bb5a1ea601846fb7dc3b798de412c3b9
RicoViking9000 <38385704+RicoViking9000@users.noreply.github.com>
2021-04-12 23:48:54 -0400

    patch 0.2.26
    Fixed bugs preventing the bot from booting when configuring data storage for new servers due to improper variable boolean logic syntax

--- Bot/database.py
+++ Bot/database.py
@@ -179,9 +179,9 @@ async def VerifyServer(s: discord.Guild, b: commands.Bot, newOnly=False, full=Fa
         'offset': -4 if serv is None or serv.get('offset') is None else serv.get('offset'), #Distance from UTC time
         'tzname': 'EST' if serv is None or serv.get('tzname') is None else serv.get('tzname'), #Custom timezone name (EST by default)
         'jumpContext': True if serv is None or serv.get('jumpContext') is None else serv.get('jumpContext'), #Whether to provide context for posted message jump URL links
-        'undoSuppression': True or serv.get('undoSuppression'), #Whether to enable the undo functionality after a message's embed was collapsed
-        'redditComplete': serv.get('redditComplete') or 1, #Link to subreddits when /r/Reddit format is typed in a message. 0 = disabled, 1 = link only, 2 = link + embed
-        'redditEnhance': serv.get('redditEnhance') if type(serv.get('redditEnhance')) is tuple else (True, True), #0: submission, 1: subreddit
+        'undoSuppression': True if not serv else serv.get('undoSuppression', True), #Whether to enable the undo functionality after a message's embed was collapsed
+        'redditComplete': 1 if not serv else serv.get('redditComplete', 1), #Link to subreddits when /r/Reddit format is typed in a message. 0 = disabled, 1 = link only, 2 = link + embed
+        'redditEnhance': (True, True) if not serv else serv.get('redditEnhance') if type(serv.get('redditEnhance')) is tuple else (True, True), #0: submission, 1: subreddit
         'birthday': 0 if serv is None or serv.get('birthday') is None else serv.get('birthday'), #Channel to send birthday announcements to
         'birthdate': datetime.datetime(2020, 1, 1, 12 + (-5 if serv is None or serv.get('offset') is None else serv.get('offset'))) if serv is None or serv.get('birthdate') is None else serv.get('birthdate'), #When to send bday announcements
         'birthdayMode': 2 if serv is None or serv.get('birthdayMode') is None else serv.get('birthdayMode'), #How to respond to automatic messages
@@ -227,7 +227,7 @@ async def VerifyServer(s: discord.Guild, b: commands.Bot, newOnly=False, full=Fa
             'ageKickOwner': False if serv is None or spam.get('ageKickOwner') is None else spam.get('ageKickOwner'),
             'ageKickWhitelist': [] if serv is None or spam.get('ageKickWhitelist') is None else spam.get('ageKickWhitelist'),
             'timedEvents': [] if serv is None or spam.get('timedEvents') is None else spam.get('timedEvents'), #Bans, mutes, etc
-            'automuteRole': spam.get('automuteRole') or 0},
+            'automuteRole': 0 if not spam else spam.get('automuteRole', 0)},
         "cyberlog": {
             # 'globalSettings': vars(loggingHome),
             "enabled": False if log is None or log.get('enabled') is None else log.get('enabled'),
