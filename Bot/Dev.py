a7d7b88e4f9c736d2b7391f85fc5398008f077df
RicoViking9000 <ricoviking9000@gmail.com>
2024-05-13 16:36:44 -0400

    refactor into GroupCog
    commands now only show up in Disguard server

--- Bot/Dev.py
+++ Bot/Dev.py
@@ -16,35 +16,33 @@ def team_check():
         return ctx.interaction.user in (await ctx.bot.application_info()).team.members
     return commands.check(predicate)
 
-class Dev(commands.Cog):
+@team_check()
+@app_commands.guilds(utility.DISGUARD_SERVER_ID)
+class Dev(commands.GroupCog, name='dev', description='Dev-only commands'):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.emojis: dict[str, discord.Emoji] = bot.get_cog('Cyberlog').emojis
-
-    @commands.group(fallback='help', invoke_without_command=True)
-    async def dev(self, ctx: commands.Context):
-        '''View or execute dev commands'''
-        pass
+        super().__init__()
     
-    @dev.command(name='verify_database', guild=discord.Object(utility.DISGUARD_SERVER_ID))
+    @app_commands.command(name='verify_database')
     async def verify_database(self, ctx: commands.Context):
         '''Verify the database'''
         await ctx.send('Verifying database...')
         await ctx.send('Database verified!')
 
-    @dev.command(name='index_server', guild=discord.Object(utility.DISGUARD_SERVER_ID))
+    @app_commands.command(name='index_server')
     async def index_server(self, ctx: commands.Context):
         '''Index the server'''
         await ctx.send('Indexing server...')
         await ctx.send('Server indexed!')
     
-    @dev.command(name='index_channel', guild=discord.Object(utility.DISGUARD_SERVER_ID))
+    @app_commands.command(name='index_channel')
     async def index_channel(self, ctx: commands.Context):
         '''Index the channel'''
         await ctx.send('Indexing channel...')
         await ctx.send('Channel indexed!')
     
-    @dev.command(name='eval', guild=discord.Object(utility.DISGUARD_SERVER_ID))
+    @app_commands.command(name='eval')
     async def eval(self, ctx: commands.Context, *, code: str):
         '''Evaluate code'''
         code = textwrap.dedent(code)
@@ -68,12 +66,12 @@ class Dev(commands.Cog):
         else:
             await ctx.send(f'```py\n{result}\n```')
     
-    @dev.command(name='get_log_file', guild=discord.Object(utility.DISGUARD_SERVER_ID))
+    @app_commands.command(name='get_log_file')
     async def get_log_file(self, ctx: commands.Context):
         '''Get the log file'''
         await ctx.send(file=discord.File('discord.log'))
 
-    @dev.command(name='sync')
+    @app_commands.command(name='sync')
     async def sync_tree(self, ctx: commands.Context):
         '''Sync the tree'''
         print('syncing...')
@@ -82,7 +80,7 @@ class Dev(commands.Cog):
         await ctx.send('Synced tree')
         print('synced')
 
-    @dev.command(name='clear_commands', guild=discord.Object(utility.DISGUARD_SERVER_ID))
+    @app_commands.command(name='clear_commands')
     async def clear_commands(self, ctx: commands.Context):
         '''Clear the command cache'''
         await self.bot.tree.clear_commands()
