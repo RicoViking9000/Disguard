876ee85530ca4d7bf63ee32611aa8fb4f7301aa7
RicoViking9000 <ricoviking9000@gmail.com>
2024-05-14 19:25:37 -0400

    refactor dev commands to use interactions
    dev commands mostly complete, autocomplete added but channel autocomplete with arg not finished, some new dev commands (like restart cogs) still need to be added

--- Bot/Dev.py
+++ Bot/Dev.py
@@ -1,12 +1,15 @@
 '''Cog that contains Disguard's dev-only commands'''
 
+import asyncio
 import inspect
+import typing
 import discord
 from discord import app_commands
 from discord.ext import commands
 import utility
 import Support
 import textwrap
+import database
 # =============================================================================
 
 def team_check():
@@ -25,36 +28,54 @@ class Dev(commands.GroupCog, name='dev', description='Dev-only commands'):
         super().__init__()
     
     @app_commands.command(name='verify_database')
-    async def verify_database(self, ctx: commands.Context):
+    async def verify_database(self, interaction: discord.Interaction):
         '''Verify the database'''
-        await ctx.send('Verifying database...')
-        await ctx.send('Database verified!')
-
+        await interaction.response.send_message('Verifying database...')
+        await database.Verification(self.bot)
+        await interaction.edit_original_response(content='Database verified!')
+        
     @app_commands.command(name='index_server')
-    async def index_server(self, ctx: commands.Context):
-        '''Index the server'''
-        await ctx.send('Indexing server...')
-        await ctx.send('Server indexed!')
+    async def index_server(self, interaction: discord.Interaction, *, server_arg: typing.Optional[str]):
+        '''Index a server's messages'''
+        if server_arg: servers: list[discord.Guild] = [self.bot.get_guild(int(server_arg))]
+        else: servers: list[discord.Guild] = self.bot.guilds
+        await interaction.response.send_message(f'Indexing [{",  ".join([str(s)[:15] for s in servers])}]...')
+        for server in servers:
+            await asyncio.gather(*[indexMessages(server, channel, True) for channel in server.text_channels])
+        await interaction.edit_original_response(content='Server indexed!')
+
+    @index_server.autocomplete('server_arg')
+    async def index_server_autocomplete(self, interaction: discord.Interaction, argument: str):
+        if argument: return [app_commands.Choice(name=str(server[0]), value=str(server[0].id)) for server in utility.FindServers(self.bot.guilds, argument)][:25]
+        return [app_commands.Choice(name=str(server), value=str(server.id)) for server in self.bot.guilds][:25]
     
     @app_commands.command(name='index_channel')
-    async def index_channel(self, ctx: commands.Context):
-        '''Index the channel'''
-        await ctx.send('Indexing channel...')
-        await ctx.send('Channel indexed!')
+    async def index_channel(self, interaction: discord.Interaction, channel_arg: str):
+        '''Index a channel'''
+        channel = self.bot.get_channel(int(channel_arg))
+        await interaction.response.send_message(f'Indexing {channel.name}...')
+        await indexMessages(channel.guild, channel, True)
+        await interaction.edit_original_response(content='Channel indexed!')
+
+    @index_channel.autocomplete('channel_arg')
+    async def index_channel_autocomplete(self, interaction: discord.Interaction, argument: str):
+        text_channels = [channel for channel in self.bot.get_all_channels() if isinstance(channel, discord.TextChannel)]
+        if argument: return [app_commands.Choice(name=channel[0].name, value=str(channel[0].id)) for channel in utility.FindChannels(text_channels, argument)][:25]
+        return [app_commands.Choice(name=str(channel), value=str(channel.id)) for channel in self.bot.get_all_channels() if isinstance(channel, discord.TextChannel)][:25]
     
     @app_commands.command(name='eval')
-    async def eval(self, ctx: commands.Context, *, code: str):
+    async def eval(self, interaction: discord.Interaction, *, code: str):
         '''Evaluate code'''
         code = textwrap.dedent(code)
         env = {
             'bot': self.bot,
-            'ctx': ctx,
+            'interaction': interaction,
             'discord': discord,
             'commands': commands,
             'utility': utility,
             'Support': Support,
             'textwrap': textwrap,
-            'self': self
+            'self': self,
         }
         env.update(globals())
         try:
@@ -62,31 +83,55 @@ class Dev(commands.GroupCog, name='dev', description='Dev-only commands'):
             if inspect.isawaitable(result):
                 result = await result
         except Exception as e:
-            await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+            await interaction.response.send_message(f'```py\n{e.__class__.__name__}: {e}\n```')
         else:
-            await ctx.send(f'```py\n{result}\n```')
+            await interaction.response.send_message(f'```py\n{result}\n```')
     
     @app_commands.command(name='get_log_file')
-    async def get_log_file(self, ctx: commands.Context):
+    async def get_log_file(self, interaction: discord.Interaction):
         '''Get the log file'''
-        await ctx.send(file=discord.File('discord.log'))
+        await interaction.response.send_message(file=discord.File('discord.log'))
 
     @app_commands.command(name='sync')
-    async def sync_tree(self, ctx: commands.Context):
+    async def sync_tree(self, interaction: discord.Interaction):
         '''Sync the tree'''
         print('syncing...')
         await self.bot.tree.sync()
         await self.bot.tree.sync(guild=discord.Object(utility.DISGUARD_SERVER_ID))
-        await ctx.send('Synced tree')
+        await interaction.response.send_message('Synced tree')
         print('synced')
 
     @app_commands.command(name='clear_commands')
-    async def clear_commands(self, ctx: commands.Context):
+    async def clear_commands(self, interaction: discord.Interaction):
         '''Clear the command cache'''
         await self.bot.tree.clear_commands()
         await self.bot.tree.sync()
-        await ctx.send('Cleared tree')
+        await interaction.response.send_message('Cleared tree')
 
+
+async def indexMessages(server: discord.Guild, channel: discord.TextChannel, full=False):
+    if channel.id in (534439214289256478, 910598159963652126): return
+    start = datetime.datetime.now()
+    try: saveImages = (await utility.get_server(server))['cyberlog'].get('image') and not channel.is_nsfw()
+    except AttributeError: return
+    if lightningdb.database.get_collection(str(channel.id)) is None: full = True
+    existing_message_counter = 0
+    async for message in channel.history(limit=None, oldest_first=full):
+        try: await lightningdb.post_message(message)
+        except mongoErrors.DuplicateKeyError:
+            if not full:
+                existing_message_counter += 1
+                if existing_message_counter >= 15: break
+        if not message.author.bot and (discord.utils.utcnow() - message.created_at).days < 7 and saveImages:
+            attachments_path = f'Attachments/{message.guild.id}/{message.channel.id}/{message.id}'
+            try: os.makedirs(attachments_path)
+            except FileExistsError: pass
+            for attachment in message.attachments:
+                if attachment.size / 1000000 < 8:
+                    try: await attachment.save(f'{attachments_path}/{attachment.filename}')
+                    except discord.HTTPException: pass
+        if full: await asyncio.sleep(0.0015)
+    print(f'Indexed {server.name}: {channel.name} in {(datetime.datetime.now() - start).seconds} seconds')
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Dev(bot))
