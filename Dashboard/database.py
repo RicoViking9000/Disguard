cb6844bb65ff2a46795eae841be7195a3635d5ea
RicoViking9000 <38385704+RicoViking9000@users.noreply.github.com>
2024-08-05 01:42:36 -0400

    0.6.16 fix web dashboard (#5)
    * spruce up dashboard & fix broken auth
    
    seems like outdated mongoDB credentials led to infinite looping with not being able to log into the dashboard due to a generic try/catch block. that logic has been fixed to catch more specific errors without looping
    
    had to disable flask breadcrumbs due to an error. hopefully this will still work in prod
    
    added proper secret key to flask session manager to sign browser cookies. also, updated session length to 30 days (after that, you'll need to re-auth with discord before accessing the dashboard)
    
    ruff formatting got applied here too
    
    add dashboard specific requirements.txt
    
    * re-enable breadcrumbs & spruce up auth page
    
    * fix moderation context menu

--- Dashboard/database.py
+++ Dashboard/database.py
@@ -1,24 +1,25 @@
-'''This file manages database operations after data is saved in the web dashboard'''
-import pymongo
-import motor.motor_asyncio
-import oauth
-import dns
-from flask import request
+"""This file manages database operations after data is saved in the web dashboard"""
 
-mongo = pymongo.MongoClient(oauth.mongo()) #Database connection URL in another file so you peeps don't go editing the database ;)
+import pymongo
+
+import oauth
+
+mongo = pymongo.MongoClient(oauth.mongo())  # Database connection URL in another file so you peeps don't go editing the database ;)
 db = mongo.disguard
 servers = db.servers
 
+
 def UpdateMemberWarnings(server: int, warnings: int):
-    '''Web dashboard equivalent of ../bot/dashboard's version. More calculations are done here
+    """Web dashboard equivalent of ../bot/dashboard's version. More calculations are done here
     server: ID of server
-    warnings: dashboard's new warnings variable'''
+    warnings: dashboard's new warnings variable"""
     serv = servers.find_one({'server_id': server})
     members = serv.get('members')
     update_operations = []
     for member_id, member_data in members.items():
         newWarnings = warnings
-        if member_data.get('warnings', 3) == 0: newWarnings = 0
+        if member_data.get('warnings', 3) == 0:
+            newWarnings = 0
         elif member_data.get('warnings') != serv.get('antispam').get('warn'):
             newWarnings = serv.get('antispam').get('warn') - member_data.get('warnings')
         update_operations.append(pymongo.UpdateOne({'server_id': server}, {'$set': {f'members.{member_id}': newWarnings}}))
